5e179a4e52fe0b2ed88d49de1dd4eb7d409f7e1f
maker957 <383679422@qq.com>
2019-03-03 12:48:31 +0800

    ValidParentheses_20_3_3

---
+++ src/VaLidParentheses_20_3_3/ValidParentheses.java
@@ -0,0 +1,32 @@
+package VaLidParentheses_20_3_3;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch=='(') {
+                stack.push(')');
+            }
+            else if (ch=='[') {
+                stack.push(']');
+            }
+            else if (ch=='{') {
+                stack.push('}');
+            }
+            else {
+                if (stack.isEmpty()||ch!=stack.pop())
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
