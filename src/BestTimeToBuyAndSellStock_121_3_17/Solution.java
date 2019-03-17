9a885f67ee96dcf90aa01bcf642689440e8cf430
maker957 <383679422@qq.com>
2019-03-17 21:07:13 +0800

    3_17

---
+++ src/BestTimeToBuyAndSellStock_121_3_17/Solution.java
@@ -0,0 +1,13 @@
+﻿package BestTimeToBuyAndSellStock_121_3_17;
+
+class Solution {
+    public int maxProfit(int[] prices){
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for(int i = 0; i<prices.length;i++){
+            minPrice = Math.min(minPrice, prices[i]);//找到当前时刻的最小值
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);//使用当前最大值减去当前最小值
+        }
+        return maxProfit;
+    }
+}
---
+++ src/BestTimeToBuyAndSellStock_II_122_3_17/Solution.java
@@ -0,0 +1,17 @@
+﻿package BestTimeToBuyAndSellStock_II_122_3_17;
+
+class Solution{
+    /**
+     * 解题思路
+     * 将每个后一个比前一个大的组合，将后一个减去前一个的值加到总和当中
+     * 
+     */
+    public int maxProfit(int[] prices){
+        int total = 0;
+        for (int i = 0; i < prices.length-1; i++) {
+            if (prices[i+1]>prices[i])
+                total += prices[i+1] -prices[i];
+        }
+        return total;
+    }
+}
