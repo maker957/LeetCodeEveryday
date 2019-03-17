882bd3faa1385599c3b7f873d44619883eb54889
maker957 <383679422@qq.com>
2019-03-17 15:01:11 +0800

    3_17

--- src/ConvertSortedArrayToBinarySearchTree_108_3_17/Solution.java
+++ src/ConvertSortedArrayToBinarySearchTree_108_3_17/Solution.java
@@ -1,7 +1,25 @@
 ﻿package ConvertSortedArrayToBinarySearchTree_108_3_17;
 
 public class Solution {
+    /**
+     * 解题思路：使用递归将树不断的分为左子树右子树
+     * 二分法
+     */
     public TreeNode sortedArrayToBST(int[] nums){
-        
+        if(nums == null || nums.length ==0) return null;
+
+        TreeNode head = helper(nums, 0, nums.length-1); 
+        return head;
+    }
+
+    public TreeNode helper(int[] num,int left, int right){
+        if(left > right){
+            return null;
+        }
+        int mid = (left +right) / 2;
+        TreeNode node = new TreeNode(num[mid]);
+        node.left = helper(num, left, mid-1);
+        node.right = helper(num, mid + 1, right);
+        return node;
     }
 }
---
+++ src/PascalTriangle_118_3_17/Solution.java
@@ -0,0 +1,42 @@
+﻿package PascalTriangle_118_3_17;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 本题为输出杨辉三角形
+ * ArrayList存放每一行的的数字。
+ * allrows为整个三角形
+ * 思路不懂。
+ * Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+ */
+class Soution {
+    /**
+     * 
+     * 解题思路：
+     */
+    public List<List<Integer>> generage(int numRows) {
+
+        List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+        ArrayList<Integer> row = new ArrayList<>();
+
+        for(int i = 0; i < numRows; i++){
+            row.add(0, 1);//设置行数和初始数字
+
+            for (int j = 1; j < row.size()-1; j++)
+                row.set(j, row.get(j) + row.get(j+1));
+
+            allrows.add(new ArrayList<Integer>(row));
+        }
+        return allrows;
+    }
+}
