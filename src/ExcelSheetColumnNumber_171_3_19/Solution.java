bebbee67acc0c47bff46b73b9c66cff9ec33880f
maker957 <383679422@qq.com>
2019-03-19 12:56:31 +0800

    3_19

---
+++ src/ExcelSheetColumnNumber_171_3_19/Solution.java
@@ -0,0 +1,31 @@
+﻿package ExcelSheetColumnNumber_171_3_19;
+
+
+/**
+ *  A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+
+    Input: "AB"
+    Output: 28
+
+    Input: "ZY"
+    Output: 701
+ */
+class Solution {
+    /**
+     * 26进制转化
+     */
+    public int titleToNumber(String s){
+        int result = 0;
+        for(int i = 0; i < s.length(); i++){
+            result = result * 26 + (s.charAt(i) - 'A' +1);
+        }
+        return result;
+    }
+}
---
+++ src/MajotityElement_169_3_19/Solution.java
@@ -0,0 +1,33 @@
+﻿package MajotityElement_169_3_19;
+
+
+    /**
+     * 摩尔投票法
+     * 
+     * 输入：{1,2,1,3,1,1,2,1,5}
+     * 
+     * major = nums[0] =1,count = 1
+     * 2 != major count--   count->0
+     * count =0, count++ major = nums[2] = 1
+     * 。。。
+     * 
+     */
+class Solution {
+    
+    public int majorityElement(int[] nums){
+        if(nums == null) return 0;
+
+        int major=nums[0],count = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(count==0){
+                count++;
+                major = nums[i];
+            }else if(major == nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return major;
+    }
+}
