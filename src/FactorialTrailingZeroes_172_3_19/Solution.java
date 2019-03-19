8a53edfb8a702b61669dace8c5bb9f8413cf71fc
maker957 <383679422@qq.com>
2019-03-19 20:06:38 +0800

    3_19

---
+++ src/FactorialTrailingZeroes_172_3_19/Solution.java
@@ -0,0 +1,12 @@
+﻿package FactorialTrailingZeroes_172_3_19;
+
+class Solution {
+    /**
+     * 求阶乘含有几个零。
+     */
+    public int trialingZeroes(int n){
+
+        //
+        return n/5 == 0 ? 0 : n / 5 + trialingZeroes(n / 5);
+    }
+}
