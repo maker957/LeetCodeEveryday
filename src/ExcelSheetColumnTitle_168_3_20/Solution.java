034611d95a56c4961d5fa89d3000208c1fc781c7
maker957 <383679422@qq.com>
2019-03-20 23:29:20 +0800

    3_20

---
+++ src/ExcelSheetColumnTitle_168_3_20/Solution.java
@@ -0,0 +1,19 @@
+﻿package ExcelSheetColumnTitle_168_3_20;
+
+class Solution {
+
+    /**
+     * 10进制数转26进制数
+     */
+    public String converToTitle(int n){
+        StringBuilder result = new StringBuilder();
+
+        while(n > 0){
+            n--;
+            result.insert(0, (char)('A'+n%26));
+            n /= 26;
+        }
+
+        return result.toString();
+    }
+}
---
+++ src/SecondHigestSalary_176_3_20/Solution.sql
@@ -0,0 +1,16 @@
+﻿
+Write a SQL query to get the second highest salary from the Employee table.
+
+/*
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+*/
+
+select(select DISTINCT Salary from Employee ORDER By Salary DESC limit 1 offset 1)
