027aea2a5c76d77532dc7d3c0c439caecbd26637
maker957 <383679422@qq.com>
2019-03-26 23:45:35 +0800

    3_26

---
+++ src/ContainsDuplicateII_219_3_26/Solution.java
@@ -0,0 +1,29 @@
+﻿package ContainsDuplicateII_219_3_26;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+
+class Solution {
+    public boolean containsNearByDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i =0;i<nums.length;i++){
+            if(i<k) set.remove(nums[i-k-1]);
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
+    }
+
+    public boolean containsNearByDuplicate1(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i]))
+                if (i - map.get(nums[i]) <= k) return true;
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
