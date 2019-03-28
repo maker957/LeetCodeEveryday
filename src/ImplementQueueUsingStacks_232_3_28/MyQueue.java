9d7f19771640109ebaeab9c0b8f23e27ec2ccf0d
maker957 <383679422@qq.com>
2019-03-28 14:59:08 +0800

    3_28

---
+++ src/ImplementQueueUsingStacks_232_3_28/MyQueue.java
@@ -0,0 +1,40 @@
+﻿package ImplementQueueUsingStacks_232_3_28;
+
+import java.util.Stack;
+
+class MyQueue {
+
+    Stack<Integer> input = new Stack<>();
+    Stack<Integer> output = new Stack<>();
+    
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return output.pop();
+    }
+
+    /**
+     * 
+     * @return 返回队列顶端元素
+     */
+    public int peek() {
+        if(output.empty()){
+            while(!input.empty()){
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+
+}
--- src/PowerOfTwo_231_3_27/Solution.java
+++ src/PowerOfTwo_231_3_27/Solution.java
@@ -1,10 +1,18 @@
 ﻿package PowerOfTwo_231_3_27;
 
 class Solution {
+
+    /**
+     * 2的幂次的2进制肯定只有一个1
+     * 例如0010  0100 1000
+     */
     public boolean isPowerOfTwo(int n){
         return n>0 && Integer.bitCount(n) == 1;
     }
 
+    /**
+     * 二的幂对2取余肯定为零
+     */
     public boolean isPowerOfTwo2(int n){
         if(n == 1){
             return true;
@@ -14,6 +22,10 @@ class Solution {
         }
         return false;
     }
+
+    /**
+     * 通过位运算求num中1的个数
+     */
     public boolean isPowerOfTwo3(int n){
         if(n <= 0) return false;
         return countBit(n) == 1;
