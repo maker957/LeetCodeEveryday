ef303b8f5bfb1c54f35252d23c49f330feb1dfdd
maker957 <383679422@qq.com>
2019-03-30 00:47:49 +0800

    3_29

---
+++ src/PalindromeLinkedList_234_3_29/ListNode.java
@@ -0,0 +1,9 @@
+﻿package PalindromeLinkedList_234_3_29;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+    }
+}
---
+++ src/PalindromeLinkedList_234_3_29/Solution.java
@@ -0,0 +1,42 @@
+﻿package PalindromeLinkedList_234_3_29;
+
+
+
+public class Solution {
+    public boolean isPalindrome(ListNode head){
+        //移动slow和next
+        ListNode fast = head, slow = head;
+        while(fast != null &&  fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        //奇数个节点
+        if(fast != null){
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while(slow != null){
+            if(fast.val != slow.val){
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+        
+    }
+}
