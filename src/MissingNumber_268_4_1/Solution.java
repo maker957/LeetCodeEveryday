a7dfc7f36efc13207eda8ba24a12ed4f9549a7fa
maker957 <383679422@qq.com>
2019-04-01 16:41:09 +0800

    4_1

---
+++ src/MissingNumber_268_4_1/Solution.java
@@ -0,0 +1,30 @@
+﻿package MissingNumber_268_4_1;
+
+class Solution {
+    /**
+     * 异或求不存在的那个数。
+     */
+    public int missingNumber(int[] nums){
+        int xor = 0, i = 0;
+        for (i = 0; i < nums.length; i++){
+            xor = xor ^ i ^ nums[i];
+        }
+
+        return xor ^ i;//?
+    }
+
+    /**
+     * 求等差数列的和减去每一项
+     * @param nums
+     * @return
+     */
+    public int missingNumber1(int[] nums){
+        int len = nums.length;
+        int sum = (0 + len)*(len+1)/2;
+
+        for (int i = 0; i < len; i++) {
+            sum-=nums[i];
+        }
+        return sum;
+    }
+}
---
+++ src/MoveZeroes_283_4_1/Solution.java
@@ -0,0 +1,22 @@
+﻿package MoveZeroes_283_4_1;
+
+class Solution {
+    /**
+     * 首先将不为0的数字加入数组，后将所有的0加入数组
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return;
+
+        int insertPos = 0;
+        for (int num : nums) {
+            if (num != 0)
+                nums[insertPos++] = num;//不为零的数
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;//0
+        }
+    }
+}
