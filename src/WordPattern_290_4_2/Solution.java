173dea0fd0e7058d753abfacdfb5ce1de39c45d5
maker957 <383679422@qq.com>
2019-04-02 23:44:41 +0800

    4_2

---
+++ src/WordPattern_290_4_2/Solution.java
@@ -0,0 +1,22 @@
+﻿package WordPattern_290_4_2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean wordPattern(String pattern, String str){
+        
+        String[] words = str.split(" ");
+        if (words.length != pattern.length()){
+            return false;
+        }
+
+        Map index = new HashMap<>();
+        for (Integer i = 0; i < words.length; ++i) {
+            if(index.put(pattern.charAt(i), i) != index.put(words[i], i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
