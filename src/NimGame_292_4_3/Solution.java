a64d7a43f1df403e317f3eb46596213ee8390923
maker957 <383679422@qq.com>
2019-04-04 09:17:47 +0800

    4_4

---
+++ src/NimGame_292_4_3/Solution.java
@@ -0,0 +1,10 @@
+﻿package NimGame_292_4_3;
+
+class Solution {
+    public boolean canWinNim(int n){
+    if (n <= 0) {
+        throw new IllegalArgumentException();
+    }
+    return !(n % 4 == 0);
+    }
+}
