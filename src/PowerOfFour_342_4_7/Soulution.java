bc4cacc9b3784c65b220eddf984336e3602dca3b
maker957 <383679422@qq.com>
2019-04-07 23:15:54 +0800

    4_7

---
+++ src/PowerOfFour_342_4_7/Soulution.java
@@ -0,0 +1,7 @@
+﻿package PowerOfFour_342_4_7;
+
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 && (num & 0x55555555) != 0;
+    }
+}
---
+++ src/ReverseString_344_4_7/Solution.java
@@ -0,0 +1,31 @@
+﻿package ReverseString_344_4_7;
+
+class Solution {
+    /**
+     * 将字符串转化为字符数组,前后互相交换
+     * 
+     * @param s
+     * @return new Reverse String
+     */
+    public String reverseString(String s) {
+        char[] word = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char temp = word[i];
+            word[i] = word[j];
+            word[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(word);
+    }
+
+    public void reverseString2(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - i - 1] = temp;
+        }
+    }
+}
