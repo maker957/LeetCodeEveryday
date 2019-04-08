fa8eaccb8ab96019f94d33f160521207202633c7
maker957 <383679422@qq.com>
2019-04-08 19:04:13 +0800

    4_8

---
+++ src/IntersectionOfTwoArrays_349_4_8/Solution.java
@@ -0,0 +1,29 @@
+﻿package IntersectionOfTwoArrays_349_4_8;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
+
+        for (int i = 0; i < nums1.length; i++){
+            set.add(i);
+        }
+
+
+        for (int i = 0; i < nums2.length; i++) { //数组的长度用length not length()
+            if(set.contains(nums2[i])){
+                intersect.add(nums2[i]);
+            }
+        }
+
+        int[] result = new int[intersect.size()]; //集合的大小用size()
+        int i = 0; //赋值时使用
+        for (Integer num : intersect) {
+            result[i++] = num; //数组赋值 result[i++] = num
+        }
+        return result;
+    }
+}
---
+++ src/ReverseVowelsOfASttring_345_4_8/Solution.java
@@ -0,0 +1,64 @@
+﻿package ReverseVowelsOfASttring_345_4_8;
+
+class Solution {
+    /**
+     * 交换元音字母的位置
+     * @param s
+     * @return 交换后的字符串
+     */
+    public String reverseVowels(String s) {
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' }; //所有的元音字母
+        boolean[] set = new boolean[256];// 256位的数组
+        for (char c : vowels)
+            set[(int) c] = true;//
+        char[] ar = s.toCharArray();
+        int len = s.length();
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            if (!set[(int) ar[left]])
+                left++;
+            if (!set[(int) ar[right]])
+                right--;
+            else if (set[(int) ar[left]] && set[(int) ar[right]]) {
+                char temp = ar[left];
+                ar[left] = ar[right];
+                ar[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(ar);
+
+    }
+
+    public String reverseVowels2(String s){
+        String vowels = "aoeiuAOEIU";
+        char[] a = s.toCharArray();
+        int i = 0;
+        int j = a.length - i -1;
+        while (i < j){
+            while(i < j && !vowels.contains(a[i] + "")){
+                i++;
+            }
+            while(i < j && !vowels.contains(a[j] + "")){
+                j--;
+            }
+            if(i < j){
+                char c = a[i];
+                a[i++] = a[j];
+                a[j--] = c;
+            }
+        }
+        return new String(a);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.reverseVowels2("hello").equals("holle"));
+        System.out.println(s.reverseVowels2("leetcode").equals("leotcede"));
+        System.out.println(s.reverseVowels2("").equals(""));
+        System.out.println(s.reverseVowels2("aA").equals("Aa"));
+        System.out.println(s.reverseVowels2("Live on evasions? No I save no evil.").equals("Live on evasIons? No i save no evil."));
+    }
+}
