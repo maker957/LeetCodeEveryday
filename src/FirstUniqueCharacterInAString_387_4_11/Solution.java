7c5d8d548dd9d2408c785f45972a6ec04dabc5c0
maker957 <383679422@qq.com>
2019-04-11 14:40:23 +0800

    4_11

---
+++ src/FirstUniqueCharacterInAString_387_4_11/Solution.java
@@ -0,0 +1,46 @@
+﻿package FirstUniqueCharacterInAString_387_4_11;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    /**
+     * jiance重复字母的出现数，如果第一个不重复出现的i的数量为1，则返回i
+     * @param s
+     * @return index
+     */
+    public int firstUniqChar1(String s){
+        int freq[] = new int[26];
+        for(int i = 0; i < s.length();i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 
+     */
+    public int firstUniqChar2(String s){
+        Map<Character, Integer> charMap = new HashMap<>(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            if (!charMap.containsKey(s.charAt(i))) {
+                charMap.put(s.charAt(i), i);//首次出现
+            }
+            else{
+                charMap.put(s.charAt(i), charMap.get(s.charAt(i)) + 1); //非首次出现 + 1
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (charMap.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/RansomNote_383_4_11/Solution.java
@@ -0,0 +1,21 @@
+﻿package RansomNote_383_4_11;
+
+class Solution {
+    /*
+    * 将一个字母出现的次数进行统计，然后用ransomNote中出现的字符减去 magazine中的字母数量
+    * 如果够减则说明ransomNote中的字符一定出现在magazine中。否则返回true
+    */
+    public boolean canConstruct(String ransomNote, String magazine){
+        int[] arr = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            arr[magazine.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i< ransomNote.length(); i++){
+            if(--arr[ransomNote.charAt(i) - 'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
