978022b3391bad7a03193bb63a285b29580691b5
maker957 <383679422@qq.com>
2019-04-12 13:38:48 +0800

    4_12

---
+++ src/BinaryWatch_401_4_12/Solution.java
@@ -0,0 +1,20 @@
+﻿package BinaryWatch_401_4_12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> readBinaryWatch(int num) {
+        List<String> times = new ArrayList<>();
+
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount(h * 64 + m) == num) {
+                    times.add(String.format("%d:%02d", h, m));
+                }
+            }
+        }
+        return times;
+
+    }
+}
---
+++ src/NthDigit_400_4_12/Solution.java
@@ -0,0 +1,50 @@
+﻿package NthDigit_400_4_12;
+
+class Solution {
+    /**
+     * 看不懂，待
+     * @param n
+     * @return
+     */
+    public int findNthDigit(int n){
+        //数字的长度
+        int len = 1;
+        //该数之前的所有数字所占数量 
+        long count = 9;
+
+        int start = 1;
+
+        while (n > len *count) {
+            n -= len * count;
+            len += 1;
+            count *= 10;
+            start *= 10;
+        }
+
+        start += (n - 1) / len;
+        String s = Integer.toString(start);
+        return Character.getNumericValue(s.charAt((n - 1) % len));
+    }
+
+    public int findNthDigit2(int n){
+        int digit = 1;
+        long totalDigit = 0;
+
+        while(true){
+            long top = totalDigit + digit * 9 * (long)Math.pow(10, digit -1);
+            if(n >= totalDigit && n <= top){
+                break;
+            }
+
+            totalDigit = top;
+            digit++;
+        }
+
+        int start = (int)Math.pow(10, digit - 1);
+        int ret = 0;
+        totalDigit += 1;
+        int value = start + (n- (int)totalDigit)/digit;
+        ret = Integer.toString((int)value).charAt((int)((n-totalDigit) % digit)) - '0';
+        return ret;
+    }
+}
