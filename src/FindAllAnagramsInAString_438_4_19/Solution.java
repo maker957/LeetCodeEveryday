abf74899ba54ac2e67bc94a65165246fbc1f98c3
maker957 <383679422@qq.com>
2019-04-19 22:24:01 +0800

    4_19

---
+++ src/FindAllAnagramsInAString_438_4_19/Solution.java
@@ -0,0 +1,23 @@
+﻿package FindAllAnagramsInAString_438_4_19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findAngrams(String s,String p) {
+        
+        ArrayList<Integer> soln = new ArrayList<>();
+
+        if(s.length() == 0|| p.length() == 0 || s.length() < p.length()){
+            return new ArrayList<Integer>();
+        }
+
+        int[] chars = new int[26];
+        for (Character c : p.toCharArray()) {
+
+            chars[c-'a']++;
+            
+        }
+        return 0;
+    }
+}
