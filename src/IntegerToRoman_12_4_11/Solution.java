57bb929fca5015abb02ca640baf6fa7f293fb8a8
maker957 <383679422@qq.com>
2019-04-24 22:07:50 +0800

    4_24

--- src/IntegerToRoman_12_4_11/Solution.java
+++ src/IntegerToRoman_12_4_11/Solution.java
@@ -12,18 +12,25 @@ class Solution {
         return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
     }
 
-    public String intToRoman2(int num){
+    public static String intToRoman2(int num){
         int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
 
         String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
         StringBuilder sb = new StringBuilder();
+
+        //循环values数组一遍
         for (int i = 0; i < values.length; i++) {
-            while (num >= values[i]) {
-                num -= values[i];
-                sb.append(strs[i]);
+            while (num >= values[i]) { //为什么是大于等于
+                num -= values[i]; //减去要添加入字符串中的值
+                sb.append(strs[i]);//加入罗马数字
             }
         }
         return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int a = 501;
+        System.out.println(intToRoman2(a));
     }
 }
---
+++ src/ThreeSum_15_4_24/Solution.java
@@ -0,0 +1,40 @@
+﻿package ThreeSum_15_4_24;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        //选取一个base值，如果下一个值与base值重复 则跳过这个值
+        for (int i = 0; i + 2 < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1, k = nums.length - 1; //左右指针
+            int target = -nums[i]; //取负，
+            while (j < k) {
+                if (nums[j] + nums[k] == target) { //相等则加入res
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) //左指针的下一个值是否重复
+                        j++;
+                    while (j < k && nums[k] == nums[k - 1]) //右指针的值是否重复
+                        k++;
+
+                } else if (nums[j] + nums[k] > target) {  //大
+                    k--;
+                } else { //小
+                    j++;
+                }
+
+            }
+        }
+        return res;
+    }
+}
